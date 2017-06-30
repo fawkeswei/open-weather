4bb8e054aeecaeee61e976942dec9796a7cb4907
Fawkes Wei <fawkeswei@gmail.com>
2017-06-30 15:17:01 +0800

    Setup up city list UI & city weather view UI

---
+++ open-weather/CityListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CityListTableViewController.h
+//  open-weather
+//
+//  Created by Fawkes Wei on 30/06/2017.
+//  Copyright © 2017 Fawkes Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityListTableViewController : UITableViewController
+
+@end
